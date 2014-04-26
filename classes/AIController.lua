fb963b3cfa899e44d042f9c3b3f3d3491bc865a8
Joe Mycock <joe.mycock88@gmail.com>
2014-04-27 00:01:13 +0100

    dodge

--- classes/AIController.lua
+++ classes/AIController.lua
@@ -33,7 +33,8 @@ class "AIController"
 
 	isDown = function(self, keyname)
 		if keyname == "up" then
-			return self.enemyMech.pos.y < self.playerMech.pos.y and self.enemyMech.pos.y - self.playerMech.pos.y < -30
+			return self:shouldJump()
+			--return self.enemyMech.pos.y < self.playerMech.pos.y and self.enemyMech.pos.y - self.playerMech.pos.y < -30
 		elseif keyname == "down" then
 			return self.enemyMech.pos.y > self.playerMech.pos.y and self.enemyMech.pos.y - self.playerMech.pos.y > 30
 		end
@@ -41,6 +42,20 @@ class "AIController"
 		return false
 	end,
 	
+	shouldJump = function(self)
+		if self.enemyMech.projectile then
+			if self.enemyMech.projectile.pos.y > FLOORHEIGHT - 50 then
+				if self.playerMech.pos.y - self.enemyMech.projectile.pos.y > -25 then
+					return true
+				end
+			end
+		end
+		
+		return false
+			
+	end,
+	
 	update = function(self, dt)
+	
 	end,
 }
