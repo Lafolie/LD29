be22443e9e0390efb69dca938d48733bbdc76735
Bart van Strien <bart.bes@gmail.com>
2014-04-26 15:43:12 +0200

    Add slither, a basic conf.lua and a shitty main.lua, oh and a gitignore

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.title = "The bestest LD29 game evar"
+	t.author = "Lafolie, Joe, bartbes"
+	t.version = "0.9.1"
+end
---
+++ main.lua
@@ -0,0 +1,5 @@
+class = require "lib.slither" -- BOO, globals
+
+function love.draw()
+	love.graphics.rectangle("fill", 50, 50, 50, 50)
+end
