83559f4b475f68f2fb6f56ef0dbe04b103e33638
Bart van Strien <bart.bes@gmail.com>
2014-04-26 20:16:19 +0200

    Add Shader class

---
+++ classes/Shader.lua
@@ -0,0 +1,23 @@
+class "Shader"
+{
+	__init__ = function(self, filename)
+		local src = love.filesystem.read(filename)
+		self.shader = love.graphics.newShader(src)
+		print(self.shader:getWarnings())
+
+		self.uniforms = setmetatable({},
+			{
+				__newindex = function(_, name, value)
+					self.shader:send(name, value)
+				end,
+			})
+	end,
+
+	apply = function(self)
+		love.graphics.setShader(self.shader)
+	end,
+
+	unapply = function(self)
+		love.graphics.setShader()
+	end,
+}
