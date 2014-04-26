1ddf8929975331c430db37154af532d8f62ba385
Dale James <lafoliex7@aol.com>
2014-04-26 23:57:33 +0100

    The sea is no longer purple.

--- main.lua
+++ main.lua
@@ -104,7 +104,7 @@ function love.draw()
 
 	love.graphics.setColor(0xEA, 0xB9, 0x88)
 	love.graphics.rectangle("fill", 0, 0, 400, 300)
-	love.graphics.setColor(80, 70, 190)
+	love.graphics.setColor(25, 50, 120)
 	love.graphics.rectangle("fill", 0, 0, 400, FLOORHEIGHT)
 
 	for i, v in registry.entities.iterate() do
