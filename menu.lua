0ae12b0ede96d58fea067ea64f6211552a4c2a78
Bart van Strien <bart.bes@gmail.com>
2014-04-27 17:35:25 +0200

    Menus must be colourful

--- menu.lua
+++ menu.lua
@@ -9,12 +9,18 @@ local options = {
 
 local selection = 1
 local xOff, yOff, xTarget, yTarget, t
+local r, g, b, rTarget, gTarget, bTarget
 
 function menu.load()
 	xOff, yOff = 0, 0
 	xTarget, yTarget = 0, 0
+	r, g, b = 255, 255, 255
+	rTarget, gTarget, bTarget = 255, 255, 255
+
 	t = Timer("periodic", 0.050, function()
 		xTarget, yTarget = love.math.random()*6-3, love.math.random()*6-3
+
+		rTarget, gTarget, bTarget = love.math.random(0, 255), love.math.random(0, 255), love.math.random(0, 255)
 	end)
 end
 
@@ -22,10 +28,14 @@ function menu.update(dt)
 	t:update(dt)
 	xOff = xOff - (xOff-xTarget)*dt
 	yOff = yOff - (yOff-yTarget)*dt
+	r = r - (r-rTarget)*dt
+	g = g - (g-gTarget)*dt
+	b = b - (b-bTarget)*dt
 end
 
 function menu.draw()
 	love.graphics.scale(4, 4)
+	love.graphics.setColor(r, g, b)
 	love.graphics.printf("BESTU GEMU", xOff, yOff + 20, 200, "center")
 	for i, v in ipairs(options) do
 		local str = v
